71b7854a003995140d88f1cd38b36fc4db4c271e
Tim Brust <ratchet.player@gmx.de>
2017-08-10 22:46:00 +0200

    chore: mavenize project

--- src/main/java/de/dustplanet/timetracker/TimeTracker.java
+++ src/main/java/de/dustplanet/timetracker/TimeTracker.java
@@ -12,8 +12,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
-import net.milkbowl.vault.Vault;
-import net.milkbowl.vault.economy.Economy;
+// import net.milkbowl.vault.Vault;
+// import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -29,15 +29,15 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import de.bananaco.bpermissions.api.ApiLayer;
-import de.bananaco.bpermissions.api.CalculableType;
+// import de.bananaco.bpermissions.api.ApiLayer;
+// import de.bananaco.bpermissions.api.CalculableType;
 
 public class TimeTracker extends JavaPlugin implements Listener {
     private HashMap<String, Long[][]> trackedPlayers = new HashMap<String, Long[][]>();
     private HashMap<String, Long> joinedTime = new HashMap<String, Long>();
     private String[] dayValues = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
     private File time;
-    private Economy economy;
+    // private Economy economy;
 
     public void onDisable() {
 	for (Player p : getServer().getOnlinePlayers()) {
@@ -54,12 +54,12 @@ public class TimeTracker extends JavaPlugin implements Listener {
     @SuppressWarnings("unchecked")
     public void onEnable() {
 	// Check for Vault
-	Plugin vault = getServer().getPluginManager().getPlugin("Vault");
-	if (vault != null && vault instanceof Vault) {
-	    // If Vault is enabled, load the economy
-	    getLogger().info("Loaded Vault successfully");
-	    setupEconomy();
-	}
+	// Plugin vault = getServer().getPluginManager().getPlugin("Vault");
+	// if (vault != null && vault instanceof Vault) {
+	//     // If Vault is enabled, load the economy
+	//     getLogger().info("Loaded Vault successfully");
+	//     setupEconomy();
+	// }
 	joinedTime.clear();
 	trackedPlayers.clear();
 	time = new File(getDataFolder(), "time.dat");
@@ -83,14 +83,14 @@ public class TimeTracker extends JavaPlugin implements Listener {
 	}
     }
 
-    // Initialized to work with Vault
-    private Boolean setupEconomy() {
-	RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-	if (economyProvider != null) {
-	    economy = economyProvider.getProvider();
-	}
-	return (economy != null);
-    }
+    // // Initialized to work with Vault
+    // private Boolean setupEconomy() {
+	// RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+	// if (economyProvider != null) {
+	//     economy = economyProvider.getProvider();
+	// }
+	// return (economy != null);
+    // }
 
     private void saveHashMap(Object fileObj, File file) {
 	FileOutputStream f;
@@ -109,14 +109,14 @@ public class TimeTracker extends JavaPlugin implements Listener {
 	Object o = null;
 	FileInputStream f;
 	try {
-	    f = new FileInputStream(file);  
+	    f = new FileInputStream(file);
 	    ObjectInputStream s = new ObjectInputStream(f);
-	    o = s.readObject();         
+	    o = s.readObject();
 	    s.close();
 	}
 	catch (FileNotFoundException e) {}
 	catch (IOException e) {}
-	catch (ClassNotFoundException e) {}  
+	catch (ClassNotFoundException e) {}
 	return o;
     }
 
@@ -129,30 +129,30 @@ public class TimeTracker extends JavaPlugin implements Listener {
 	joinedTime.put(name, System.currentTimeMillis());
 	Player player = event.getPlayer();
 	World world = player.getWorld();
-	String group = ApiLayer.getGroups(world.getName(), CalculableType.USER, name)[0];
-	if (group.equalsIgnoreCase("normal")) {
-	    calculate(name);
-	    joinedTime.put(name, System.currentTimeMillis());
-	    Long[][] playerTime = trackedPlayers.get(name);
-	    long allTimePlayTime = 0;
-	    for (Long[] week : playerTime) {
-		long playTime = 0;
-		for (Long day : week) {
-		    if (day == null) continue;
-		    playTime += day;
-		}
-		allTimePlayTime += playTime;
-	    }
-	    int milliseconds = (int) allTimePlayTime;
-	    int hours   = (int) ((milliseconds / (1000*60*60)) % 24);
-	    if (hours >= 7) {
-		player.sendMessage(ChatColor.GREEN + "Glückwunsch, du hast mehr als 7 Stunden gespielt und bist nun automatisch MITGLIED");
-		//player.sendMessage(ChatColor.YELLOW + "Du kannst nun 2 Homes haben und deine Base protecten. Hierzu einfach /dustplanet machen!");
-		ApiLayer.setGroup(world.getName(), CalculableType.USER, name, "mitglied");
-		economy.depositPlayer(name, 100);
-		player.sendMessage(ChatColor.GREEN + "Dir wurden ausserdem 100 Dusties gutgeschrieben!");
-	    }
-	}
+	// String group = ApiLayer.getGroups(world.getName(), CalculableType.USER, name)[0];
+	// if (group.equalsIgnoreCase("normal")) {
+	//     calculate(name);
+	//     joinedTime.put(name, System.currentTimeMillis());
+	//     Long[][] playerTime = trackedPlayers.get(name);
+	//     long allTimePlayTime = 0;
+	//     for (Long[] week : playerTime) {
+	// 	long playTime = 0;
+	// 	for (Long day : week) {
+	// 	    if (day == null) continue;
+	// 	    playTime += day;
+	// 	}
+	// 	allTimePlayTime += playTime;
+	//     }
+	//     int milliseconds = (int) allTimePlayTime;
+	//     int hours   = (int) ((milliseconds / (1000*60*60)) % 24);
+	//     if (hours >= 7) {
+	// 	player.sendMessage(ChatColor.GREEN + "GlÃ¼ckwunsch, du hast mehr als 7 Stunden gespielt und bist nun automatisch MITGLIED");
+	// 	//player.sendMessage(ChatColor.YELLOW + "Du kannst nun 2 Homes haben und deine Base protecten. Hierzu einfach /dustplanet machen!");
+	// 	ApiLayer.setGroup(world.getName(), CalculableType.USER, name, "mitglied");
+	// 	economy.depositPlayer(name, 100);
+	// 	player.sendMessage(ChatColor.GREEN + "Dir wurden ausserdem 100 Dusties gutgeschrieben!");
+	//     }
+	// }
     }
 
     @EventHandler
@@ -174,15 +174,15 @@ public class TimeTracker extends JavaPlugin implements Listener {
     public boolean onCommand (CommandSender sender, Command command, String commandLabel, String[] args) {
 	if (args.length == 0) {
 	    sender.sendMessage(ChatColor.GREEN + "TimeTracker version 1.0.2 by xGhOsTkiLLeRx");
-	    sender.sendMessage(ChatColor.RED + "Für die Benutzung tippe bitte /tracker help|hilfe|h");
+	    sender.sendMessage(ChatColor.RED + "FÃ¼r die Benutzung tippe bitte /tracker help|hilfe|h");
 	}
 	else {
 	    if (args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("hilfe") || args[0].equalsIgnoreCase("h")) {
 		if (sender.hasPermission("TimeTracker.help")) {
 		    sender.sendMessage(ChatColor.GREEN + "TimeTracker version 1.0.2");
 		    sender.sendMessage(ChatColor.YELLOW + "/tracker help|hilfe|h" + ChatColor.WHITE+ " - Zeigt diese Hilfe an");
-		    sender.sendMessage(ChatColor.YELLOW + "/tracker get|g <SpielerName>" + ChatColor.WHITE+ " - Gibt einen Überblick über die Spielstunden pro Kalenderwoche");
-		    sender.sendMessage(ChatColor.YELLOW + "/tracker get|g <SpielerName> <KW>" + ChatColor.WHITE+ " - Gibt einen Überblick über die Spielstunden in der angegebenen Kalenderwoche");
+		    sender.sendMessage(ChatColor.YELLOW + "/tracker get|g <SpielerName>" + ChatColor.WHITE+ " - Gibt einen Ãœberblick Ã¼ber die Spielstunden pro Kalenderwoche");
+		    sender.sendMessage(ChatColor.YELLOW + "/tracker get|g <SpielerName> <KW>" + ChatColor.WHITE+ " - Gibt einen Ãœberblick Ã¼ber die Spielstunden in der angegebenen Kalenderwoche");
 		}
 		else sender.sendMessage(ChatColor.RED + "Du hast nicht die Berechtigung hierzu!");
 	    }
@@ -262,7 +262,7 @@ public class TimeTracker extends JavaPlugin implements Listener {
 		}
 		else sender.sendMessage(ChatColor.RED + "Du hast nicht die Berechtigung hierzu!");
 	    }
-	    else sender.sendMessage(ChatColor.RED + "Dieses Argument ist unbekannt. Tippe für die Hilfe /tracker help|hilfe");
+	    else sender.sendMessage(ChatColor.RED + "Dieses Argument ist unbekannt. Tippe fÃ¼r die Hilfe /tracker help|hilfe");
 	}
 	return true;
     }
